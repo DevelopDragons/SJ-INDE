11f8107a9b0e2f81bd6ba699e5da404b42fc0002
yeseokim <callikys@naver.com>
2026-06-18 22:21:20 +0900

    vercel 배포 위한 작업

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,35 @@
-import mysql from 'mysql2/promise';
+// MySQL 활용
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-});
+// import mysql from 'mysql2/promise';
 
-// MySQL 연결
-(async () => {
-    try {
-        const connection = await pool.getConnection();
-        console.log("MySQL 연결 성공");
-        connection.release();
-    } catch (err) {
-        console.error("MySQL 연결 실패: ", err);
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: Number(process.env.DB_PORT),
+// });
+
+// // MySQL 연결
+// (async () => {
+//     try {
+//         const connection = await pool.getConnection();
+//         console.log("MySQL 연결 성공");
+//         connection.release();
+//     } catch (err) {
+//         console.error("MySQL 연결 실패: ", err);
+//     }
+// })();
+
+// export default pool;
+
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false //
     }
-})();
+});
 
 export default pool;
